0ee49726d049f1dc1bdf7c9a262129248a8220b5
Kuronoma <dlovito3@gmail.com>
2024-11-03 11:47:19 -0500

    Hola

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Plus, Edit2, Trash2 } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
 import { Db,auth} from '../Firebase';
+//Se cambio el de firebase
 import { collection, getDocs,deleteDoc,doc ,updateDoc} from 'firebase/firestore';
 import { Productores } from '../Interfaces/Interfaces';
 
